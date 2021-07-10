815da6bdd82a1d1dbfa92118007857af115ad06d
Kanwar Pal Singh <40355127+kanwarps@users.noreply.github.com>
2021-07-09 20:08:51 -0400

    Update parallax.js

--- JS/parallax.js
+++ JS/parallax.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
 
+	if ($(window).width() > 640) { //set up breaking point
 	//Parallax Animations
 
 	var scene = document.getElementById('parallax-1');
 	var parallaxInstance = new Parallax(scene, {
 		relativeInput: true
 	});
+	}
+	
 });
