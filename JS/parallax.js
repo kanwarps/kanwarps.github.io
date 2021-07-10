389bd96ff7e3b11d97e08e776c37fc66197acb4a
Kanwar Pal Singh <40355127+kanwarps@users.noreply.github.com>
2021-07-09 20:18:57 -0400

    Adding Files

---
+++ JS/parallax.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+	//Parallax Animations
+
+	var scene = document.getElementById('parallax-1');
+	var parallaxInstance = new Parallax(scene, {
+		relativeInput: true
+	});
+});
