dbc79e6ebc5bdc9f913befe97a0158883d6a70aa
Kanwar Pal Singh <40355127+kanwarps@users.noreply.github.com>
2021-07-09 20:16:50 -0400

    Revert "Adding the assets"
    This reverts commit a896673fd55c9b1804f83859c9b7c6b8776170b7.

--- JS/parallax.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-	//Parallax Animations
-
-	var scene = document.getElementById('parallax-1');
-	var parallaxInstance = new Parallax(scene, {
-		relativeInput: true
-	});
-});
