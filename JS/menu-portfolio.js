a1c837e94b9157c6d3bfa50fc2ddb79eeee84dd7
Gurek Singh <gurek15033@iiitd.ac.in>
2021-07-26 20:58:19 +0530

    Update menu-portfolio.js

--- JS/menu-portfolio.js
+++ JS/menu-portfolio.js
@@ -1,15 +1,5 @@
 $(document).ready(function () {
 
-    // //Menu
-    $('.nmenu input').change(function () {
-        document.getElementById('nmenu-bg').classList.toggle('change-bg');
-        document.getElementById('nnav').classList.toggle('nnav-change');
-    });
-
-    $('.nnav a').on('click', function () {
-        $('.nmenu input').click();
-    });
-
     function addGray() {
         $('.navbar-brand').addClass('color-gray');
         $('.nmenu-bg').addClass('bg-gray');
@@ -18,7 +8,6 @@ $(document).ready(function () {
         $('.desktop-menu li a').addClass('color-gray')
 
     };
-    // addGray();
 
     function removeGray() {
         $('.navbar-brand').removeClass('color-gray');
@@ -27,29 +16,14 @@ $(document).ready(function () {
         $('.nnav li a').removeClass('color-white');
     };
 
-    // Handling the menu color in other pages than the home
-    $(window).on('scroll', function () {
-        logoPosition = $('.navbar-brand').offset().top;
-        stripAPosition = $('.stripA').offset().top;
-        if (logoPosition > stripAPosition) {
-            addGray();
-        } else {
-            removeGray();
-        }
-    });
-
     $(window).on('scroll', function () {
         logoPosition = $('.navbar-brand').offset().top;
         strip2Position = $('.strip2').offset().top;
-        strip3Position = $('.strip3').offset().top;
-        if (logoPosition > strip3Position) {
+        if (logoPosition > strip2Position) {
             addGray();
-        } else if (logoPosition > strip2Position) {
-            removeGray();
         } else {
             removeGray();
         }
     });
-
 
 });
